125f7717a11629d82ec71a4a2ee3adfef0353b40
Ahmed Osman <engineer.ahmedfarouk@gmail.com>
2024-07-30 14:20:13 -0300

    add utils

---
+++ src/cnnClassifier/utils/common.py
@@ -0,0 +1,146 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from cnnClassifier import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+
+# @ensure_annotations : it checks the types of arguments and return values against the annotations, raising an error if they do not match.
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """
+    Reads a YAML file and returns its content as a ConfigBox.
+
+    Args:
+        path_to_yaml (Path): Path to the YAML file.
+
+    Raises:
+        ValueError: If the YAML file is empty.
+        Exception: If any other error occurs during file reading.
+
+    Returns:
+        ConfigBox: Parsed content of the YAML file.
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("YAML file is empty")
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """
+    Creates a list of directories if they do not already exist.
+
+    Args:
+        path_to_directories (list): List of directory paths to create.
+        verbose (bool, optional): If True, logs directory creation. Defaults to True.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """
+    Saves data as a JSON file.
+
+    Args:
+        path (Path): Path to the JSON file.
+        data (dict): Data to be saved in the JSON file.
+    """
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"JSON file saved at: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """
+    Loads data from a JSON file.
+
+    Args:
+        path (Path): Path to the JSON file.
+
+    Returns:
+        ConfigBox: Data loaded from the JSON file as a ConfigBox.
+    """
+    with open(path) as f:
+        content = json.load(f)
+    logger.info(f"JSON file loaded successfully from: {path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """
+    Saves data as a binary file.
+
+    Args:
+        data (Any): Data to be saved as a binary file.
+        path (Path): Path to the binary file.
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info(f"Binary file saved at: {path}")
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """
+    Loads data from a binary file.
+
+    Args:
+        path (Path): Path to the binary file.
+
+    Returns:
+        Any: Data loaded from the binary file.
+    """
+    data = joblib.load(path)
+    logger.info(f"Binary file loaded from: {path}")
+    return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """
+    Gets the size of a file in kilobytes (KB).
+
+    Args:
+        path (Path): Path to the file.
+
+    Returns:
+        str: Size of the file in kilobytes.
+    """
+    size_in_kb = round(os.path.getsize(path) / 1024)
+    return f"~ {size_in_kb} KB"
+
+def decodeImage(imgstring, fileName):
+    """
+    Decodes a base64 string and saves it as an image file.
+
+    Args:
+        imgstring (str): Base64 encoded image string.
+        fileName (str): Path to save the decoded image file.
+    """
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+
+def encodeImageIntoBase64(croppedImagePath):
+    """
+    Encodes an image file into a base64 string.
+
+    Args:
+        croppedImagePath (str): Path to the image file to be encoded.
+
+    Returns:
+        str: Base64 encoded image string.
+    """
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())

9a7328d89db2d5d9d0c5c892864b0682c829c550
Ahmed Osman <engineer.ahmedfarouk@gmail.com>
2024-07-31 04:04:30 -0300

    edit data ingestion

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,7 +1,4 @@
 from pathlib import Path
 
-# Get the absolute path to the current script's directory
-BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
-
-CONFIG_FILE_PATH = BASE_DIR / 'config/config.yaml'
-PARAMS_FILE_PATH = BASE_DIR / 'params.yaml'
+CONFIG_FILE_PATH = Path('config\config.yaml')
+PARAMS_FILE_PATH = Path('params.yaml')
